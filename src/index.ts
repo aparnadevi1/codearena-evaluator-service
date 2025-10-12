2b0ccf3fe8a21505fbc82d0bfcae9e0ffd9e4160
Aparna <chodapuneediaparnadevi@gmail.com>
2025-10-12 19:27:47 +0530

    basic-api-setup-done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
 
 import serverConfig from "./config/serverConfig.js";
+import apiRouter from "./routes/index.js";
 const app: Express = express(); //if we dont mention also express() function returns the types
 
+app.use("/api", apiRouter);
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started at PORT :${serverConfig.PORT}`);
   console.log("wow");
